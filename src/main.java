03a613de02a366a6ef1552b5b2d3136932dfe28c
Mahmoudd1 <mahmouddsoky6@gmail.com>
2023-04-11 20:04:40 +0200

    Added main methode

--- src/main.java
+++ src/main.java
@@ -1,3 +1,7 @@
 public class main {
-
+    public static void main(String[] args) {
+        Solution sol=new Solution();
+        System.out.println(sol.removeStars("leet**cod*e")); // input s = "leet**cod*e" Output: "lecoe"
+        System.out.println(sol.removeStars("erase*****")); // input s = "erase*****" Output: ""
+    }
 }
